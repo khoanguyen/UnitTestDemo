48cdcfa9bc16a395fc89e2f855c3281e09cecde5
meothailand <sunnyvo@gmail.com>
2013-06-24 19:57:08 -0700

    Add Users table

---
+++ ShoppingWebDB/Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[UserId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserName] NVARCHAR(20) NOT NULL, 
+    [Password] NVARCHAR(20) NOT NULL, 
+    [Point] INT NOT NULL
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_Users_UserName] ON [dbo].[Users] ([UserName])
